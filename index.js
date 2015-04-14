ec9085d35da5608b9bd8939fcd138883554cccf5
Luca Capra <luca.capra@gmail.com>
2015-04-14 22:50:11 +0200

    fix crud ops

--- index.js
+++ index.js
@@ -8,29 +8,64 @@ var server = require('./lib/server'),
 
 config.load(require('./config.json'));
 
-db.connection().then(function(c) {
-    c.collection('coll').find({}).toArray(function(err, res) {
-        console.log(err, res);
-    });
-});
-
 server.start().then(function(app) {
 
     var _get = function(req, res) {
         logger.debug("Read param");
         db.get(req.params)
-            .then(function(data) {
-                logger.debug("Got", data);                
-                res.status(200).send(data);
+            .then(function(record) {
+                
+                var key = req.params.param;
+                var val;
+                
+                if(record) {
+                    if(key) {
+                        val = record[ key ];
+                    }
+                    else {
+                        val = {};
+                        Object.keys(record).forEach(function(field) {
+                            if( field === '_id' || field === 'group' ) return;
+                            val[field] = record[field];
+                        });
+                    }
+                    
+                    logger.debug("Got result", val);
+                }
+                else {
+                    logger.debug("Value is empty", record);
+                }
+                
+                
+                res.status(200).send(val);
             })
             .catch(function(err) {
+                logger.error(err);
                 res.status(500).send(err);
             });
         
     };
     
     var _create = function(req, res) {
-        logger.debug("Set param");
+        
+        logger.debug("Create param", JSON.stringify(req.body));
+        db.remove(req.params).then(function() {
+            
+            db.set(req.params, req.body)
+                .then(function(data) {
+                    res.status(200).send(data);
+                })
+                .catch(function(err) {
+                    res.status(500).send(err);
+                });
+
+        });
+    };
+    
+    var _update = function(req, res) {
+        
+        logger.debug("Update param", JSON.stringify(req.body));
+        
         db.set(req.params, req.body)
             .then(function(data) {
                 res.status(200).send(data);
@@ -51,9 +86,24 @@ server.start().then(function(app) {
             });
     };
 
-    app.get('/:userid/:collection/:param', _get);
-    app.post('/:userid/:collection/:param', _create);
-    app.put('/:userid/:collection/:param', _create);
-    app.delete('/:userid/:collection/:param', _delete);
+    app.get('/:collection/:group/:param', _get);
+    app.get('/:collection/:group', _get);
+
+    app.post('/:collection/:group/:param', _create);
+    app.post('/:collection/:group', _create);
     
+    app.put('/:collection/:group/:param', _update);
+    app.put('/:collection/:group', _update);
+
+    app.delete('/:collection/:group/:param', _delete);
+    app.delete('/:collection/:group', _delete);
+    
+    
+});
+
+process.on('SIGINT', function() {
+    db.disconnect().then(function() {
+        logger.debug("Disconnected");
+        process.exit(0);
+    });
 });
